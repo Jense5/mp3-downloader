87e23e28eedd0885b615c070a5ec87f98b086f0b
Jensen Bernard <jensenbernard5@gmail.com>
2017-04-23 11:48:09 +0200

    Add services file

---
+++ src/service.js
@@ -0,0 +1,12 @@
+// @flow
+
+import Youtube from 'youtube-node';
+import { iTunes } from 'itunes-info';
+
+const fetchFromiTunes = options => new Promise((resolve, reject) => {
+
+});
+
+const fetchFromYoutube = options => new Promise((resolve, reject) => {
+
+});
