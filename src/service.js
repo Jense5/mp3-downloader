fb95b03851614a474f68c8ef8d261712dedcba48
JochenBe <jochenbernard@gmail.com>
2017-04-23 11:58:48 +0200

    Add YouTube partially

--- src/service.js
+++ src/service.js
@@ -8,5 +8,16 @@ const fetchFromiTunes = options => new Promise((resolve, reject) => {
 });
 
 const fetchFromYoutube = options => new Promise((resolve, reject) => {
+  const youTube = new YouTube();
+  youTube.setKey(options.token);
 
+  youTube.search(options.query, options.results, function(error, results) {
+    if (error) { return reject(new Error(error)); }
+
+    const res = results.items;
+
+    if (res.length === 0) { return reject(new Error('No YouTube results!')) }
+
+    
+  });
 });
